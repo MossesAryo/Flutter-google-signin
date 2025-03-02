52b6aabd6e1eba9d404bce161b4da1f9911e5901
MossesAryo <mossesaryobimo@gmail.com>
2025-03-02 21:50:59 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:new_firebase/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.android);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SignInScreen());
+  }
+}
+
+class SignInScreen extends StatefulWidget {
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  User? _user;
+
+  Future<void> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      if (googleUser == null) return; // User canceled sign-in
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      final OAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithCredential(credential);
+
+      setState(() {
+        _user = userCredential.user;
+      });
+    } catch (e) {
+      print("Error signing in with Google: $e");
+    }
+  }
+
+  Future<void> signOut() async {
+    await GoogleSignIn().signOut();
+    await FirebaseAuth.instance.signOut();
+    setState(() {
+      _user = null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Google Sign-In")),
+      body: Center(
+        child:
+            _user == null
+                ? ElevatedButton(
+                  onPressed: signInWithGoogle,
+                  child: const Text("Sign in with Google"),
+                )
+                : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(_user!.photoURL ?? ''),
+                      radius: 40,
+                    ),
+                    const SizedBox(height: 10),
+                    Text("Name: ${_user!.displayName}"),
+                    Text("Email: ${_user!.email}"),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: signOut,
+                      child: const Text("Sign out"),
+                    ),
+                  ],
+                ),
+      ),
+    );
+  }
+}
