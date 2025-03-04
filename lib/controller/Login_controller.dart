8c8d46e355026e1679337d53676129d313bec66f
MossesAryo <mossesaryobimo@gmail.com>
2025-03-04 08:03:26 +0700

    `Added Google sign-in functionality and login screen`

--- lib/controller/Login_controller.dart
+++ lib/controller/Login_controller.dart
@@ -1,35 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:new_firebase/firebase_options.dart';
-import 'package:new_firebase/screens/login_screen.dart';
 import 'package:new_firebase/screens/main_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.android);
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SignInScreen());
-  }
-}
-
-class SignInScreen extends StatefulWidget {
-  @override
-  _SignInScreenState createState() => _SignInScreenState();
-}
-
-class _SignInScreenState extends State<SignInScreen> {
+class Login {
   User? _user;
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  
 
   Future<void> signInWithGoogle() async {
     try {
@@ -59,12 +35,5 @@ class _SignInScreenState extends State<SignInScreen> {
     } catch (e) {
       print("Error signing in with Google: $e");
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home:LoginScreen() 
-    );
   }
 }
